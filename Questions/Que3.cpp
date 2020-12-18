9019081b14b0ec938d3a9c167e5410f84acff078
Shubham Patil <shubhampatil01021998@gmail.com>
2020-12-18 23:41:03 +0530

    done three questions

---
+++ Questions/Que3.cpp
@@ -0,0 +1,18 @@
+#include  <iostream>
+using namespace std;
+ 
+int main()
+{
+    int a,b;
+ 
+    //without cascading cout
+    cout << "Enter value of a and b : ";
+ 
+    //cascading cin
+    cin >>a >>b;
+ 
+    //cascading cout
+    cout<< "A : " << a << ", B : " << b << endl;
+ 
+    return 0;
+}
