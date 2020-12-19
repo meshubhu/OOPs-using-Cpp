f5e787ecac74512c9174885d1d4809132d30e664
meshubhu <shubhampatil01021998@gmail.com>
2020-12-19 22:54:42 +0530

    done seventh question

--- Que7.cpp
+++ Que7.cpp
@@ -1,11 +1,20 @@
-#include <iostream> 
-using namespace std; 
-inline int cube(int s) 
-{ 
-	return s*s*s; 
-} 
-int main() 
-{ 
-	cout << "The cube of 3 is: " << cube(3) << "\n"; 
-	return 0; 
-} //Output: The cube of 3 is: 27 
+#include <iostream>
+using namespace std;
+
+// Program to create functions using inline...
+inline double square(int n)
+{
+	return n*n;
+}
+
+inline float average(int n1,int n2)
+{
+	return ( (float)(n1+n2)/2 );
+}
+
+int main()
+{
+	cout<<"\n SQUARE IS  = "<< square(12);
+	cout<<"\n AVERAGE IS = "<< average(10,21)<<"\n";
+	return 0;
+}
