66e00eccadc62131f06445578aa9c8e520ac66cc
meshubhu <shubhampatil01021998@gmail.com>
2020-12-19 22:34:36 +0530

    done sixth question

---
+++ Que6.cpp
@@ -0,0 +1,19 @@
+#include  <iostream>
+using namespace std;
+ 
+int var ;
+ 
+int& fun()
+{
+    return var;
+}
+ 
+int main()
+{
+    //Function used as LVALUE
+    fun() = 10;
+ 
+    cout << "Value of var : " << var << endl; 
+ 
+    return 0;
+}
