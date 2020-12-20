5b341eb14190d9e96e081f35cd394320464ed539
meshubhu <shubhampatil01021998@gmail.com>
2020-12-20 21:56:09 +0530

    done eleventh question

--- Que11.cpp
+++ Que11.cpp
@@ -1,45 +1,31 @@
-/*C++ program to demonstrate example of function overloading.*/
+/*C++ program to read string using cin.getline().*/
 #include  <iostream>
 using namespace std;
  
-void printChar();
-void printChar( char c );
-void printChar( char c, int num );
-void printChar(int num, char c);
+//macro definitions for maximum length of variables
+#define MAX_NAME_LENGTH     50
+#define MAX_ADDRESS_LENGTH  100
+#define MAX_ABOUT_LENGTH    200
+ 
+using namespace std;
  
 int main()
 {
-    printChar();
-    printChar('#');
-    printChar(10,'$');
-    printChar('@',10);
-     
-    cout<< endl;
-         
-    return 0;
-}
+    char name[MAX_NAME_LENGTH],address[MAX_ADDRESS_LENGTH],about[MAX_ABOUT_LENGTH];
  
-void printChar()
-{
-        cout<< endl<<"%";
-}
-void printChar( char c )
-{
-        cout<< endl<< c;
-}
-void printChar( char c, int num )
-{
-    int i=0;
-         
-    cout<< endl;
-    for(i=0; i< num; i++)
-        cout<< c;
-}
-void printChar(int num, char c)
-{
-    int i=0;
-         
-    cout<< endl;
-    for(i=0; i< num; i++)
-        cout<< c;
+    cout << "Enter name: ";
+    cin.getline(name,MAX_NAME_LENGTH);
+ 
+    cout << "Enter address: ";
+    cin.getline(address,MAX_ADDRESS_LENGTH);
+ 
+    cout << "Enter about yourself (press # to complete): ";
+    cin.getline(about,MAX_ABOUT_LENGTH,'#');    //# is a delimiter 
+ 
+    cout << "\nEntered details are:";
+    cout << "Name: "  << name << endl;
+    cout << "Address: " << address << endl;
+    cout << "About: " << about << endl;
+ 
+    return 0;
 }
