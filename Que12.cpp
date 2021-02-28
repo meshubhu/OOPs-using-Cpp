f65decd4efb17e0f85703cb98cb1e47a2b69ba1b
meshubhu <shubhampatil01021998@gmail.com>
2021-02-28 17:05:07 +0530

    done twelfth question

--- Que12.cpp
+++ Que12.cpp
@@ -1,18 +1,31 @@
-/*C++ program to generate random numbers.*/
- 
-#include<iostream.h>
-#include<stdlib.h>
- 
-int main()
-{
-    int i;          //loop counter
-    int num;        //store random number
- 
-    randomize();    //call it once to generate random number
-    for(i=1;i<=10;i++)
-    {
-        num=rand()%100; //get random number
-        cout << num << "\t";
-    }
-    return 0;
+// Online C++ compiler to run C++ program online
+#include <iostream> 
+#include <stdlib.h> 
+#include <math.h> 
+
+using namespace std;
+
+const int low=15;
+
+int main( ) 
+{ 
+    int point = 5; 
+    int number = 0;
+    int random = 0;
+    int counter;
+    
+    cout << "How much do you want randome numbers ?";
+    cin >> counter;
+
+    // Seed the random function first
+    srand( 0 );
+
+    for( int i=1; i<=counter; i++ ) 
+    { 
+        random = rand() % 100; // Generate a random number on the range [0,100)
+        number = low + random; 
+
+        cout<< number << " " ; 
+        point--; 
+    } 
 }
